3c00854185d4a837db1ee476f91726e1f30773c8
Niña Jaira Lael B. Cagomoc <41723907+jairacagomoc@users.noreply.github.com>
2022-07-07 23:26:19 +0800

    ALL FEATURES ARE FUNCTIONAL

---
+++ CAGOMOC_ContactTracingQRCode1/CAGOMOC_ContactTracingQRCode1/Form1.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.txtbox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(18, 29)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(234, 228)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'txtbox1
+        '
+        Me.txtbox1.Location = New System.Drawing.Point(258, 53)
+        Me.txtbox1.Multiline = True
+        Me.txtbox1.Name = "txtbox1"
+        Me.txtbox1.Size = New System.Drawing.Size(366, 181)
+        Me.txtbox1.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Sitka Banner", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(447, 240)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(177, 29)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Exit "
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Sitka Banner", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.Location = New System.Drawing.Point(447, 20)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(177, 29)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Scan Now!"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Sitka Banner", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button3.Location = New System.Drawing.Point(258, 18)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(177, 29)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Start Camera "
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Sitka Banner", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button4.Location = New System.Drawing.Point(255, 240)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(177, 29)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "Clear Data"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.ClientSize = New System.Drawing.Size(644, 295)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtbox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "CAGOMOC_ContactTracingQRCode"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents txtbox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ CAGOMOC_ContactTracingQRCode1/CAGOMOC_ContactTracingQRCode1/Form1.vb
@@ -0,0 +1,73 @@
+﻿Imports ZXing
+Imports WebCam_Capture
+Imports MessagingToolkit.QRCode.Codec
+
+
+Public Class Form1
+    Dim iExit As DialogResult
+    Dim iClear As DialogResult
+    WithEvents mywebcam As WebCamCapture
+    Dim reader As QRCodeDecoder
+
+
+    Private Sub StartWebcam()
+        Try
+            StopWebcam()
+            mywebcam = New WebCamCapture
+            mywebcam.Start(0)
+            mywebcam.Start(0)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub StopWebcam()
+        Try
+            mywebcam = New WebCamCapture
+            mywebcam.Stop()
+            mywebcam.Dispose()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub MyWebcam_ImageCaptured(source As Object, e As WebcamEventArgs) Handles mywebcam.ImageCaptured
+        PictureBox1.Image = e.WebCamImage
+    End Sub
+    Private Sub Start_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        StartWebcam()
+        txtbox1.Clear()
+    End Sub
+
+    Private Sub Scan_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            Dim SD As New SaveFileDialog
+            StopWebcam()
+            reader = New QRCodeDecoder
+            txtbox1.Text = reader.decode(New Data.QRCodeBitmapImage(PictureBox1.Image))
+            MsgBox("QRcode is now detected!", "Contact Tracing QRCode")
+            SD.Filter = "PNG|*.png"
+            If SD.ShowDialog() = DialogResult.OK Then
+                PictureBox1.Image.Save(SD.FileName, Imaging.ImageFormat.Png)
+                MsgBox("Data Saved!", "Contact Tracing QRCode")
+            End If
+        Catch ex As Exception
+            MsgBox("QRcode is not detected!")
+            StartWebcam()
+            txtbox1.Clear()
+        End Try
+    End Sub
+
+    Private Sub Clear_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        txtbox1.Clear()
+    End Sub
+
+    Private Sub Exit_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        iExit = MessageBox.Show("Do you want to exit? ", "Contact Tracing QRCode", MessageBoxButtons.YesNo, MessageBoxIcon.Information)
+        If iExit = DialogResult.Yes Then
+            Application.Exit()
+        Else
+            'do nothing
+        End If
+    End Sub
+End Class
